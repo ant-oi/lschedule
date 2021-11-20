6250cf64b195b50ed08962cf44897514e3514436
ant-oi <wladislaw.makowski@aol.com>
2021-11-20 17:17:47 +0500

    ordinary commit

---
+++ headers/functions.h
@@ -0,0 +1,21 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <...>
+#include <...>
+#include <...>
+
+void draw( table* Table );
+
+void exit();
+
+void new_table( char** arguments );
+void assign( char** arguments );
+void del( char** arguments);
+
+void done( char** arguments );
+void undone( char** arguments );
+
+void rename( char** arguments );
+
+#endif
